d7e470a183e408bb814c63be582e471887278147
Chesney Nyffels <chesney@nyffels.be>
2022-02-26 22:40:40 +0100

    bugfix on test

--- projects/angular-components/src/lib/components/toggle-button/toggle-button.component.spec.ts
+++ projects/angular-components/src/lib/components/toggle-button/toggle-button.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PremuiToggle } from './toggle.component';
+import { PremuiToggleButton } from './toggle-button.component';
 
-describe('PremuiToggle', () => {
-  let component: PremuiToggle;
-  let fixture: ComponentFixture<PremuiToggle>;
+describe('PremuiToggleButton', () => {
+  let component: PremuiToggleButton;
+  let fixture: ComponentFixture<PremuiToggleButton>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PremuiToggle ]
+      declarations: [ PremuiToggleButton ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PremuiToggle);
+    fixture = TestBed.createComponent(PremuiToggleButton);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
